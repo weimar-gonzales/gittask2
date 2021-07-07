dd1942bdec57882e59c2365ac4ce2aec5319ef20
weimar-gonzales <weimargonzales@gmail.com>
2021-07-07 00:47:20 -0400

    program to add two numbers

--- test1.py
+++ test1.py
@@ -1,3 +1,13 @@
 # test test
 setup(name='api-test',
     version='1.0.0.dev1')
+	# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
